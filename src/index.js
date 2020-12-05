fcd45e69b58d8dbdd3fc4362b375998a1d001ffd
chinchan-mole <501849659@qq.com>
2020-12-05 22:35:02 +0800

    修复未初始化计时器问题

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import select from './js/select'
 import move from './js/move'
 import player from './js/player'
 
+// 初始化计时器
+player.init();
 // 点击触发操作
 document.querySelector('.chessContent').addEventListener('click', select.clickChess);
 // 点击移动判断并检查move是否正常完成（正常完成返回playerchange）
